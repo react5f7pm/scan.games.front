c97c6ebf75731913640385db18cf334d6df54b3e
frebern <frebern@naver.com>
2020-03-11 00:34:50 +0900

    [YG-STYLED] style: 간단한 StyledCard 구현

---
+++ src/components/styled/ui-components/StyledCard/index.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const StyledCard = styled.div`
+  border-radius: 5px;
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  background-color: white;
+  padding: 10px 15px;
+`
