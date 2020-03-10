90f2fc9388e1555fd1ad403c1001b26fadc18f96
frebern <frebern@naver.com>
2020-03-11 02:23:05 +0900

    [YG-SEARCHLIST] feature: grid view 작업

--- src/components/pages/SearchListPage/index.js
+++ src/components/pages/SearchListPage/index.js
@@ -1,9 +1,65 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
+
+import {
+  StyledCard,
+} from '../../styled/ui-components'
 
 class SearchListPage extends Component {
   render () {
-    return <>Search List Page 입니다.</>
+    return (
+      <Container>
+      <SearchList>
+        { 
+          new Array(10).fill(1).map((i, idx) =>
+            <SearchItem key={idx}>
+              <ThumbnailImg src="https://steamstore-a.akamaihd.net/public/shared/images/header/globalheader_logo.png" />
+              <ProductInfo>
+                어새신 구리드
+              </ProductInfo>
+            </SearchItem>
+          )
+        }
+      </SearchList>
+      </Container>
+    )
   }
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  border: 1px solid red;
+`
+
+const SearchList = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(160px, 1fr));
+`
+
+const SearchItem = styled(StyledCard)`
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+
+  display: flex;
+  flex-direction: column;
+
+  margin: 10px;
+  padding: 0;
+`
+
+const ThumbnailImg = styled.div`
+  background: url(${props => props.src}) no-repeat center;
+  min-width: 100%;
+  text-align: center;
+  padding-top: 100%;
+
+  border: 1px solid red;
+`
+
+const ProductInfo = styled.div`
+  padding: 10px;
+  height: 80px;
+`
 
 export default SearchListPage
--- src/components/styled/ui-components/StyledCard/index.js
+++ src/components/styled/ui-components/StyledCard/index.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 export const StyledCard = styled.div`
-  border-radius: 5px;
+  border-radius: 8px;
   box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
   background-color: white;
   padding: 10px 15px;
